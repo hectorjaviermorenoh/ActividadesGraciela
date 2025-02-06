88297924b20739b7d72a719ba7a28943c11f065f
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2025-02-06 12:01:01 -0500

    deployando Codigo

--- archivo.sh
+++ archivo.sh
@@ -1,6 +1,8 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/hectorjaviermorenoh
 
+git pull --rebase origin main
+
 # Deploy codigo en Actividades Graciela hectorjaviermorenoh
 git remote remove origin
 git remote add origin git@github.com:hectorjaviermorenoh/ActividadesGraciela.git
