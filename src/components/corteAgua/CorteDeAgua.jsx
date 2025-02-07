4aeac28b44256b5c0054cae687a95ba1f21fb3ba
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2025-02-06 19:12:27 -0500

    deployando Codigo

---
+++ src/components/corteAgua/CorteDeAgua.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./CorteDeAgua.css";
+
+const CorteDeAgua = () => {
+  return (
+    <div className="corte-container">
+      <iframe
+        src="https://drive.google.com/file/d/1SzWym-QhmfH5s_f6waLRDFZu-MPocU4U/preview?usp=sharing"
+        allowFullScreen
+        title="BlockIframe"
+        className="corte-iframe"
+      ></iframe>
+
+    </div>
+  );
+};
+
+
+
+export default CorteDeAgua;
